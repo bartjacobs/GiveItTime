d73cb485cf41c109fb1cc818f31080e81e421add
Bart Jacobs <developer@bartjacobs.com>
2016-10-05 05:43:58 +0200

    Rename ViewController to RootViewController

---
+++ CoreDataStack/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  CoreDataStack
+//
+//  Created by Bart Jacobs on 17/07/16.
+//  Copyright © 2016 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+}
