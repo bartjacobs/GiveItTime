338ab4091113b5c4f2c60f2364c1ea375e2b010a
Bart Jacobs <developer@bartjacobs.com>
2016-10-05 05:40:55 +0200

    Refactor AppDelegate

--- CoreDataStack/AppDelegate.swift
+++ CoreDataStack/AppDelegate.swift
@@ -13,32 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    let coreDataManager = CoreDataManager(modelName: "DataModel")
-
     // MARK: - Application Life Cycle
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Initialize Storyboard
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-
-        // Instantiate Initial View Controller
-        if let viewController = storyboard.instantiateInitialViewController() as? ViewController {
-            // Configure View Controller
-            viewController.coreDataManager = coreDataManager
-
-            // Set Root View Controller
-            window?.rootViewController = viewController
-        }
-        
         return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        coreDataManager.saveChanges()
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        coreDataManager.saveChanges()
     }
 
 }
